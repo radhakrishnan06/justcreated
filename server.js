297bfa0a7b231bd7dac5a08195e9eaa3b70c5ea8
radhakrishnan06 <95423404+radhakrishnan06@users.noreply.github.com>
2021-12-09 23:03:18 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+"use strict";
+const cors = require("cors");
+const morgan = require("morgan");
+const express = require("express");
+const db = require("./models/index");
+const bodyParser = require("body-parser");
+const customerRouter = require("./routes/customer.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+setTimeout(
+  () =>
+    db.sequelize
+      .authenticate()
+      .then(() => {
+        db.sequelize.sync({ force: false });
+        console.log(`Authenticated`);
+      })
+      .catch((err) => console.log(`Error occurred `, err)),
+  10000
+);
+
+const port = process.env.PORT || 8080;
+const dbPort = process.env.POSTGRES_PORT;
+
+app.use("/", customerRouter);
+
+process.on("unhandledRejection", (err) => {
+  console.log(err);
+});
+
+app.listen(port, () =>
+  console.log(
+    `server is listening at ${port} and database is running at ${dbPort}`
+  )
+);
